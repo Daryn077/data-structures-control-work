ddfef16211669515192fa12200c8f64de83b8477
FAKE419767 <elubaevasylbek850@gmail.com>
2025-12-01 15:49:25 +0500

    Add files via upload

---
+++ bak.1.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, value,):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def dfs
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Node:
+#     def _init_(self, value, left = None, right = None):
+#         self.value = value
+#         self.left = left
+#         self.right = right
+
+
+
+
+
+
+
+
+
+
+
