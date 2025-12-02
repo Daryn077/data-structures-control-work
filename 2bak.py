0bab5f547d8e83b897709c02d311a0f32ee9971d
Мерей <147086960+Merei20@users.noreply.github.com>
2025-12-02 16:02:45 +0500

    Add files via upload

--- 2bak.py
+++ 2bak.py
@@ -1,42 +1,90 @@
-# 2 тапсырма
-class node
+# # 1нұсқа
+# # 1.2 
+# class Node:
+#     def _init_(self, val):
+#         self.val = val 
+#         self.next = None
+
+# a = None(1)
+# a.next = Node(2)
+# a.next.next = Node(3)
+# a.next.next.next = Node(2)
+# a.next.next.next.next = Node(4)
+
+# def remove_val
+        
+
+# 1.1
+words = ["нан", "ар", "кек", "мұң"]
+
+hash_table = {}  
+
+for w in words:
+    key = "".join(sorted(w))  
+    if key not in hash_table:
+        hash_table[key] = []
+    hash_table[key].append(w)
+
+result = list(hash_table.values())
+
+print(result)
 
 
 
 
-# 1 тапсырма
-
-from collections import deque
-
-graph = {
-    0: [1, 2],
-    1: [3],
-    2: [3],
-    3: []
-}
-
-def bfs_level_max(graph, start, k):
-    q = deque()
-    q.append((start, 0))
-    visited = set([start])
-
-    level_nodes = []
-
-    while q:
-        node, level = q.popleft()
-
-        if level == k:
-            level_nodes.append(node)
-
-        for nei in graph[node]:
-            if nei not in visited:
-                visited.add(nei)
-                q.append((nei, level + 1))
-
-    if level_nodes:
-        return max(level_nodes)
-    return None
 
 
-k = 1
-print(k, "деңгейдегі ең үлкен мән:", bfs_level_max(graph, 0, k))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  class Node:
+# #     def __init__(self, val):
+# #         self.val = val
+# #         self.next = None
+
+# # a = Node(1)
+# # a.next = Node(2)
+# # a.next.next = Node(3)
+# # a.next.next.next = Node(2)
+# # a.next.next.next.next = Node(4)
